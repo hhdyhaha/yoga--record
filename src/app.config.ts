fd5efe2ad0c4be7d201cec2aa754dec94e05c043
hhdyhaha <1164549585@qq.com>
2025-03-16 22:34:34 +0800

    更新底部导航栏图标和选中颜色
    - 为底部导航栏添加新的图标和选中状态图标
    - 设置选中颜色为 #8A6BA6

--- src/app.config.ts
+++ src/app.config.ts
@@ -14,22 +14,31 @@ export default defineAppConfig({
   },
 
   tabBar: {
+    selectedColor: '#8A6BA6',
     list: [
       {
         pagePath: 'pages/index/index',
         text: '首页',
+        iconPath: 'assets/images/iconPath/indexIcon.png',
+        selectedIconPath: 'assets/images/iconPath/selectedIndexIcon.png',
       },
       {
         pagePath: 'pages/record/index',
         text: '记录',
+        iconPath: 'assets/images/iconPath/recordIcon.png',
+        selectedIconPath: 'assets/images/iconPath/selectedRecordIcon.png',
       },
       {
         pagePath: 'pages/stats/index',
         text: '统计',
+        iconPath: 'assets/images/iconPath/statsIcon.png',
+        selectedIconPath: 'assets/images/iconPath/selectedStatsIcon.png',
       },
       {
         pagePath: 'pages/share/index',
         text: '分享',
+        iconPath: 'assets/images/iconPath/shareIcon.png',
+        selectedIconPath: 'assets/images/iconPath/selectedShareIcon.png',
       }
     ]
   }

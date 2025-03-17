40b790a0dd4f897a01b8693db6fb79ca9340db2d
hhdyhaha <1164549585@qq.com>
2025-03-17 11:19:03 +0800

    更新首页样式和结构，添加日历组件
    - 修改 app.scss，调整 .index 类的高度和宽度
    - 在 app.ts 中引入 taro-ui 样式
    - 更新 index.scss，增加 .header 的文本对齐和行高样式
    - 在 index.tsx 中引入 AtCalendar 组件，并更新头部和内容结构

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import { AtCalendar } from "taro-ui"
+
 import './index.scss'
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
@@ -10,11 +12,16 @@ export default function Index () {
   return (
     <View className='index'>
       {/* 分为头部，内容 */}
-      <View className='header'>
-        <Text>头部</Text>
+      <View className='at-row at-row__justify--between header'>
+        <View className='at-col at-col-5'>header-left</View>
+        <View className='at-col at-col-5'>header-right</View>
       </View>
+
       <View className='content'>
-        <Text>内容</Text>
+        {/* 日历 */}
+        <View className='calendar'>
+          <AtCalendar />
+        </View>
       </View>
     </View>
   )
